db6c1b72e91dc90a521b8372832ecac441cbb2a5
Tyler Fanelli <tfanelli@redhat.com>
2023-09-19 22:40:00 -0400

    fuse: Rename DIRECT_IO_RELAX to DIRECT_IO_ALLOW_MMAP
    Although DIRECT_IO_RELAX's initial usage is to allow shared mmap, its
    description indicates a purpose of reducing memory footprint. This
    may imply that it could be further used to relax other DIRECT_IO
    operations in the future.
    
    Replace it with a flag DIRECT_IO_ALLOW_MMAP which does only one thing,
    allow shared mmap of DIRECT_IO files while still bypassing the cache
    on regular reads and writes.
    
    [Miklos] Also Keep DIRECT_IO_RELAX definition for backward compatibility.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>
    Fixes: e78662e818f9 ("fuse: add a new fuse init flag to relax restrictions in no cache mode")
    Cc: <stable@vger.kernel.org> # v6.6
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -792,8 +792,8 @@ struct fuse_conn {
 	/* Is tmpfile not implemented by fs? */
 	unsigned int no_tmpfile:1;
 
-	/* relax restrictions in FOPEN_DIRECT_IO mode */
-	unsigned int direct_io_relax:1;
+	/* Relax restrictions to allow shared mmap in FOPEN_DIRECT_IO mode */
+	unsigned int direct_io_allow_mmap:1;
 
 	/** The number of requests waiting for completion */
 	atomic_t num_waiting;
