10846d19afe950e6abe365908611b1163a34d1b0
Jingbo Xu <jefflexu@linux.alibaba.com>
2024-07-05 18:04:49 +0800

    fuse: make foffset alignment opt-in for optimum backend performance
    Sometimes the file offset alignment needs to be opt-in to achieve the
    optimum performance at the backend store.
    
    For example when ErasureCode [1] is used at the backend store, the
    optimum write performance is achieved when the WRITE request is aligned
    with the stripe size of ErasureCode.  Otherwise a non-aligned WRITE
    request needs to be split at the stripe size boundary.  It is quite
    costly to handle these split partial requests, as firstly the whole
    stripe to which the split partial request belongs needs to be read out,
    then overwrite the read stripe buffer with the request, and finally write
    the whole stripe back to the persistent storage.
    
    Thus the backend store can suffer severe performance degradation when
    WRITE requests can not fit into one stripe exactly.  The write performance
    can be 10x slower when the request is 256KB in size given 4MB stripe size.
    Also there can be 50% performance degradation in theory if the request
    is not stripe boundary aligned.
    
    Besides, the conveyed test indicates that, the non-alignment issue
    becomes more severe when decreasing fuse's max_ratio, maybe partly
    because the background writeback now is more likely to run parallelly
    with the dirtier.
    
    fuse's max_ratio	ratio of aligned WRITE requests
    ----------------	-------------------------------
    70			99.9%
    40			74%
    20			45%
    10			20%
    
    With the patched version, which makes the alignment constraint opt-in
    when constructing WRITE requests, the ratio of aligned WRITE requests
    increases to 98% (previously 20%) when fuse's max_ratio is 10.
    
    fuse: fix alignment to work with redfs ubuntu
    
    - small fix to make the fuse alignment patch work with redfs ubuntu 6.8.x
    - add writeback_control to fuse_writepage_need_send() to make
    more accurate decisions about when to skip sending data
    - fix shift number for FUSE_ALIGN_PG_ORDER
    - remove test code
    
    [1] https://lore.kernel.org/linux-fsdevel/20240124070512.52207-1-jefflexu@linux.alibaba.com/T/#m9bce469998ea6e4f911555c6f7be1e077ce3d8b4
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>
    Signed-off-by: Horst Birthelmer <hbirthelmer@ddn.com>

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -443,6 +443,8 @@ struct fuse_file_lock {
  * FUSE_OVER_IO_URING: Indicate that client supports io-uring
  * FUSE_INVAL_INODE_ENTRY: invalidate inode aliases when doing inode invalidation
  * FUSE_EXPIRE_INODE_ENTRY: expire inode aliases when doing inode invalidation
+ * FUSE_ALIGN_PG_ORDER: page order (power of 2 exponent for number of pages) for
+ *			optimal io-size alignment
  * FUSE_REQUEST_TIMEOUT: kernel supports timing out requests.
  *			 init_out.request_timeout contains the timeout (in secs)
  */
@@ -491,6 +493,9 @@ struct fuse_file_lock {
 #define FUSE_DIRECT_IO_RELAX	FUSE_DIRECT_IO_ALLOW_MMAP
 #define FUSE_ALLOW_IDMAP	(1ULL << 40)
 #define FUSE_OVER_IO_URING	(1ULL << 41)
+
+#define FUSE_ALIGN_PG_ORDER	(1ULL << 50)
+
 #define FUSE_INVAL_INODE_ENTRY  (1ULL << 60)
 #define FUSE_EXPIRE_INODE_ENTRY (1ULL << 61)
 #define FUSE_REQUEST_TIMEOUT	(1ULL << 42)
@@ -912,6 +917,9 @@ struct fuse_init_in {
 #define FUSE_COMPAT_INIT_OUT_SIZE 8
 #define FUSE_COMPAT_22_INIT_OUT_SIZE 24
 
+/*
+ * align_page_order: Number of pages for optimal IO, or a multiple of that
+ */
 struct fuse_init_out {
 	uint32_t	major;
 	uint32_t	minor;
@@ -925,8 +933,10 @@ struct fuse_init_out {
 	uint16_t	map_alignment;
 	uint32_t	flags2;
 	uint32_t	max_stack_depth;
+	uint8_t		align_page_order;
 	uint16_t	request_timeout;
-	uint16_t	unused[11];
+	uint8_t		padding;
+	uint32_t	unused[5];
 };
 
 #define CUSE_INIT_INFO_MAX 4096
